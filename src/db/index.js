7269c85959bc6899d9dab18675309533c61f1a92
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-06-25 10:57:13 +0500

    DB connection established

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connctDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected successfully !!! DB Host: ${connectionInstance.connection.host} \n`
+    );
+  } catch (err) {
+    console.log(`\n Error connecting to MongoDB: ${err.message} \n`);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+export default connctDB;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+import connctDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+connctDB();
