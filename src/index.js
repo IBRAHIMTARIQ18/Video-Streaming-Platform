142430f82c5781d536ce157a4af696eb7c29f6a4
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-07-08 20:46:35 +0500

    setup express for listening after db connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database: ${err.message}`);
+  });
