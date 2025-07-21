67983bfc6d932a5c7882b1a8ae910b72d1f9ca9d
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-07-22 00:33:43 +0500

    controller template for register user

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-  express.cors({
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   })
@@ -15,5 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
