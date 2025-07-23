09ad7c7f95f44f2e8efe0a6adddf5d8eb65c4952
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-07-23 05:22:30 +0500

    validated fields, user exists check - user register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,35 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import User from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "User registered successfully",
+  // Get user data and files from frontend
+  // Validatation of data - fields should not be empty
+  // check if user already exists through email/username
+  // check for image, check for avatar
+  // upload images and avatar to cloudinary
+  // create user object - create entry in database
+  // remove password and refresh token field from response
+  // check for user creation success?
+  // return response
+
+  const { fullName, username, email, password } = req.body;
+
+  // Validate required fields
+  if (
+    [fullName, username, email, password].some((field) => field.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // Check if user already exists
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(408, "User already exists with this username or email");
+  }
 });
 
 export { registerUser };
