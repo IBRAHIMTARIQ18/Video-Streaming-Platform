69c89cf2d9cffc2672231d2e89e567c7b2ff563f
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-07-28 08:32:26 +0500

    created login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import User from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+// Function to generate access and refresh tokens
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccesToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Error generating tokens");
+  }
+};
+
+// Register user controller
 const registerUser = asyncHandler(async (req, res) => {
   // Get user data and files from frontend
   // Validatation of data - fields should not be empty
@@ -32,46 +49,110 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(408, "User already exists with this username or email");
   }
+
+  // check for avatar and cover image
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  // Upload image and avatar to Cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // Create user object
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  // Check if user creation was successful, also password and refresh token are removed.
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "User creation failed");
+  }
+
+  // Return response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-// check for avatar and cover image
-const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
+// Login user controller
+const loginUser = asyncHandler(async (req, res) => {
+  // get user data from frontend through req.body
+  // validate required fields - email or username
+  // check if user exists through email/username
+  // check if password is matches in DB
+  // if user exists, generate access token and refresh token
+  // send cookies with tokens
+  // return response with user data and tokens
 
-if (!avatarLocalPath) {
-  throw new ApiError(400, "Avatar image is required");
-}
+  // user data through req.body
+  const { email, username, password } = req.body;
 
-// Upload image and avatar to Cloudinary
-const avatar = await uploadOnCloudinary(avatarLocalPath);
-const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  // Validate required fields
+  if (!(email || username)) {
+    throw new ApiError(400, "Email or username is required");
+  }
 
-if (!avatar) {
-  throw new ApiError(400, "Avatar file is required");
-}
+  // user exists check
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
 
-// Create user object
-const user = await User.create({
-  fullName,
-  avatar: avatar.url,
-  coverImage: coverImage?.url || "",
-  email,
-  password,
-  username: username.toLowerCase(),
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // password match check
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid password");
+  }
+
+  // Generate access and refresh tokens
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  // Updated user object without password and refresh token
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // Set cookies with tokens
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
 });
 
-// Check if user creation was successful, also password and refresh token are removed.
-const createdUser = await User.findById(user._id).select(
-  "-password -refreshToken"
-);
-
-if (!createdUser) {
-  throw new ApiError(500, "User creation failed");
-}
-
-// Return response
-return res
-  .status(201)
-  .json(new ApiResponse(200, createdUser, "User registered successfully"));
-
-export { registerUser };
+// export controllers
+export { registerUser, loginUser };
