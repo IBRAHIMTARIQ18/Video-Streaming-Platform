8acc6ae7566094145fa48365f05110e7f327c3d4
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-08-31 10:05:18 +0500

    bugfix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.headers("Authorization")?.replace("Bearer ", "");
+      req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
       throw new ApiError(401, "Access token is required");
