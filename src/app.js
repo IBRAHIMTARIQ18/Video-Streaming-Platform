ddc6b75243f623e7e6006a46156d4593e8e2816e
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2025-09-03 11:28:14 +0500

    added routes for video management

--- src/app.js
+++ src/app.js
@@ -19,9 +19,11 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.route.js";
 import healthCheckRouter from "./routes/healthcheck.route.js";
+import videoRouter from "./routes/video.route.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/videos", videoRouter);
 
 export { app };
